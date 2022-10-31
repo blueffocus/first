3069962a125558d79cd38f048afe25a05eb4a46f
blueffocus <blueffocus@gmail.com>
2022-10-31 16:45:37 +0100

    Mi aplicación basededatos

--- web/settings.py
+++ web/settings.py
@@ -120,9 +120,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, './static'),
-)
+
 
 MEDIA_URL = '/'
 # Default primary key field type
